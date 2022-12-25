e05bb7f0eb42711a6261510014cf1338b0ac142e
Evilasio Barreto de Carvalho Neto <evilasio.neto@maida.health>
2022-12-25 10:10:40 -0300

    começando projeto

---
+++ src/main/java/dev/evilasio/lukedirector/LukeDirectorApplication.java
@@ -0,0 +1,13 @@
+package dev.evilasio.lukedirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukeDirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukeDirectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evilasio/lukedirector/LukeDirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evilasio.lukedirector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukeDirectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
